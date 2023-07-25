9add17ab44c3b857b935a743c43628beaa023bac
IgorOliverX <igorferoli019@gmail.com>
2023-07-25 15:23:04 -0300

    s

--- RevisaoPOO/App.java
+++ RevisaoPOO/App.java
@@ -5,6 +5,7 @@ import RevisaoPOO.Cadastro.Animais;
 public class App {
         public static void main(String[] args) {
        Animais obj1 = new Animais("médio",22.5, "igor", "macho", "cachorro");
+       System.out.println("j");
 
     }
 }
