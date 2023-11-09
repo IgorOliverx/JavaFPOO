0cac38010a821b51d88c80c9eb75a89645a236a9
IgorOliverx <igorferoli019@gmail.com>
2023-11-09 13:39:05 -0300

    poder pegar dudu

--- AloMundo/AloMundo.java
+++ AloMundo/AloMundo.java
@@ -2,7 +2,5 @@ package AloMundo;
 
 public class AloMundo{ // Abre a Classe AloMundo
     public static void main(String[] args) {
-        System.out.print("Alô Mundo!"); // Printa Alo Mundo na tela 
-        
+        System.out.print("Alô Mundo!"); // Printa Alo Mundo na tela
     }
-}
---
+++ jdbcconnectionfactory/JdbcConnectionTest/src/main/resources/archetype-resources/src/ConnectionFactory.kt
@@ -0,0 +1,4 @@
+package `archetype-resources`.src
+
+class ConnectionFactory {
+}
